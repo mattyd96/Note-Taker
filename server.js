ddf28e4f477d56401d84c72361567f763976faf6
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-02-19 00:15:53 +1030

    set up * and /notes paths

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const {v4 : uuidv4} = require('uuid');
+const path = require('path');
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`Server start on port ${PORT}`)
